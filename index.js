0c2fc842b3f9718fac687ce43f10e84760142a90
coccigabry <coccigab@gmail.com>
2023-02-09 08:45:18 +0100

    set up completed, ready to begin

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+import 'dotenv/config'
 import express from "express";
 import cors from 'cors'
+import mongoose from "mongoose";
+
+
+mongoose.set('strictQuery', true)
+mongoose.connect(
+    `${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`, 
+    (err) => {
+        if (err) throw err
+        console.log('Connected to mongoose')
+    }
+)
 
 
 const app = express()
